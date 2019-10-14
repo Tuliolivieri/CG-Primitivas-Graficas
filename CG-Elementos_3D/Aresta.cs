fdebebbca619da20760b5ab888c877950cf004d0
Tuliolivieri <37648167+Tuliolivieri@users.noreply.github.com>
2019-10-14 13:57:48 -0300

    Add files via upload
    Falta a rotação do objeto

---
+++ CG-Elementos_3D/Aresta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_Elementos_3D
+{
+    class Aresta
+    {
+        private Vertice inicio;
+        private Vertice fim;
+
+        public Aresta() { }
+        public Aresta(Vertice inicio, Vertice fim)
+        {
+            this.inicio = inicio;
+            this.fim = fim;
+        }
+
+        internal Vertice Inicio { get => inicio; set => inicio = value; }
+        internal Vertice Fim { get => fim; set => fim = value; }
+    }
+}
---
+++ CG-Elementos_3D/Objeto3D.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_Elementos_3D
+{
+    class Objeto3D
+    {
+        private List<Face> faces;
+        private List<Vertice> vertices;
+        private List<Vertice> originais;
+
+        private double[,] ma;
+
+        public Objeto3D()
+        {
+            faces = new List<Face>();
+            vertices = new List<Vertice>();
+            originais = new List<Vertice>();
+
+            ma = geraMA();
+        }
+        public Objeto3D(List<Face> faces, List<Vertice> vertices)
+        {
+            faces = new List<Face>();
+            this.faces.AddRange(faces);
+
+            vertices = new List<Vertice>();
+            this.vertices.AddRange(vertices);
+
+            originais = new List<Vertice>();
+            this.originais.AddRange(vertices);
+
+            ma = geraMA();
+        }
+
+        internal List<Face> Faces { get => faces; set => faces = value; }
+        internal List<Face> Faces1 { get => faces; set => faces = value; }
+        internal List<Vertice> Vertices { get => vertices; set => vertices = value; }
+        internal List<Vertice> Originais { get => originais; set => originais = value; }
+
+        public void addFace(Face nova)
+        {
+            if (faces != null)
+                faces.Add(nova);
+        }
+
+        public void addVertice(Vertice novo)
+        {
+            if(originais != null && vertices != null)
+            {
+                originais.Add(novo);
+                vertices.Add(new Vertice(novo.X, novo.Y, novo.Z));
+            }
+        }
+
+        public void rotacao(int ang)
+        {
+            double[,] m = new double[3, 3];
+        }
+
+        public void translacao(int tx, int ty)
+        {
+            double[,] m = new double[3, 3];
+
+            m[0, 0] = m[1, 1] = m[2, 2] = 1;
+
+            m[0, 2] = tx;
+            m[1, 2] = ty;
+
+            ma = multiplicaMatriz(ma, m);
+
+            Vertice v, vo;
+
+            for (int i = 0; i < originais.Count; i++)
+            {
+                v = vertices.ElementAt(i);
+                vo = originais.ElementAt(i);
+                
+                v.X = (int)(vo.X * ma[0, 0] + vo.Y * ma[0, 1] + ma[0, 2]);
+                v.Y = (int)(vo.X * ma[1, 0] + vo.Y * ma[1, 1] + ma[1, 2]);
+            }
+        }
+
+        public void escala(double esc)
+        {
+            double[,] m = new double[3, 3];
+
+            m[0, 0] = m[1, 1] = esc;
+            m[2, 2] = 1;
+
+            ma = multiplicaMatriz(ma, m);
+
+            Vertice v, vo;
+
+            for (int i = 0; i < originais.Count; i++)
+            {
+                v = vertices.ElementAt(i);
+                vo = originais.ElementAt(i);
+
+                v.X = (int)(vo.X * ma[0, 0] + vo.Y * ma[0, 1] + ma[0, 2]);
+                v.Y = (int)(vo.X * ma[1, 0] + vo.Y * ma[1, 1] + ma[1, 2]);
+            }
+
+            //ma = geraMA();
+        }
+
+        public double[,] geraMA()
+        {
+            double[,] m = new double[3, 3];
+
+            m[0, 0] = m[1, 1] = m[2, 2] = 1;
+
+            return m;
+        }
+
+        private double[,] multiplicaMatriz(double[,] mat1, double[,] mat2)
+        {
+            double[,] mat = new double[3, 3];
+            int i, j, k;
+            double val;
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    val = 0;
+                    for (k = 0; k < 3; k++)
+                    {
+                        val += mat1[i, k] * mat2[k, j];
+                    }
+                    mat[i, j] = val;
+                }
+            }
+
+            return mat;
+        }
+    }
+}
